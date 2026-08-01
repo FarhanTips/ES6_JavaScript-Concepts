ea7184ecea56c393b2fc4bc2b7be12e2e6a76e73
FarhanTips <farhanakbor02@gmail.com>
2026-08-01 23:30:08 +0600

    Rest Operator

--- 1. ES6 Fundamentals/4. Spread and Rest Operator — Array Max and Array, Object Copy.js
+++ 1. ES6 Fundamentals/4. Spread and Rest Operator — Array Max and Array, Object Copy.js
@@ -48,6 +48,8 @@ console.log(arr2);
  */
 
 
+
+// spread operator   
 let arr3 = [1,2,3,4];
 let arr4 = [...arr3];     // ...arr3 (spread operator) converts the array into separate values. ...arr3 array-টাকে ভেঙে দেয়।
 let arr5 = [...arr3, 16, 32];
@@ -56,3 +58,54 @@ console.log(arr3);
 console.log(arr4);
 console.log(arr5);
 
+
+
+
+
+
+console.log(Math.max(10, 5, 105, 16, 32));
+
+let list = [10, 5, 105, 16, 32];
+console.log(Math.max(list)); // This will give Nan because একটাই argument দেওয়া হয়েছে, যেটা একটা array। Math.max() array বুঝে না, arguments expect করে।
+
+console.log(Math.max(...list));  // এখানেও ...list, array-কে আলাদা arguments-এ পরিণত করেছে।
+
+
+// for object spread operator also works
+let students = {
+  name: "Utsho",
+  age: 26,
+};
+
+let students2 = { ...students, gpa: "5.00" };
+
+students.something = "test";
+
+console.log(students);
+console.log(students2);
+
+
+// spread operator এশি use করা হয় array and object clone করার জন্য।
+
+
+
+// Rest operator
+
+function sum(a, b, c, ...restNumbers) {
+    // Rest operator বাকি (remaining) arguments-গুলোকে একটি array-এর মধ্যে নিয়ে আসে।
+  console.log(restNumbers, "restNumbers");
+  let sum = 0;
+  for(let number of restNumbers){
+    sum+=number;
+  }
+  return a + b + c + sum;
+}
+
+console.log(sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
+
+
+/**
+ * 
+ * Spread (...) = Expand (ভেঙে ছড়িয়ে দেয়)
+   Rest (...) = Collect (জমা করে)  
+ */
