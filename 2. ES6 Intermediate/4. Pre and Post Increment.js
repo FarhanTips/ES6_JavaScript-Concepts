25e9a59ced31f4947aaa68ae78872c0ff5cc7281
FarhanTips <farhanakbor02@gmail.com>
2026-08-04 21:07:55 +0600

    Pre and post increment

---
+++ 2. ES6 Intermediate/4. Pre and Post Increment.js
@@ -0,0 +1,29 @@
+
+
+// post increment
+// Uses the current value then increases the value
+
+let a = 4;
+console.log(a++);
+console.log(a);
+
+let b = 17;
+let d = b++;
+console.log(d);
+console.log(b);
+
+
+
+
+
+// pre increment
+// Increases the value first then uses new value
+
+let x = 30;
+console.log(++x);
+console.log(x);
+
+let y = 58;
+let z = ++y;
+console.log(z);
+console.log(y);
