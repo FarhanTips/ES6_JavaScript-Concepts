25873296ba4b06ea80ec3f81fb6641ff30b5e9c3
FarhanTips <farhanakbor02@gmail.com>
2026-08-04 20:27:00 +0600

    Pass by value and reference

---
+++ 2. ES6 Intermediate/3. Pass by value and reference.js
@@ -0,0 +1,107 @@
+
+
+
+// Pass by Value (Pass by primitive data-type)
+
+function check(name, age){
+    name = "New Name";
+    age++;
+    console.log("Inside function-----------------------");
+    console.log(name, age);
+}
+
+let myName = "Farhan";
+let myAge = 24;
+console.log("Outside function, before going to function-----------------------");
+console.log(myName, myAge);
+
+check(myName, myAge);
+
+console.log("Outside function, After returning from function-----------------------");
+console.log(myName, myAge);
+
+// No changes in myName and myAge because primitive data-type passes a copy of the value not the memory reference.
+
+
+
+
+
+
+
+
+
+
+// Pass by Reference (Pass by non-primitive data-type) Mutate inside function
+
+function check1(obj){
+    obj.name = "Aree montu";
+    obj.marks.push(35);
+    console.log("Inside function-----------------------");
+    console.log(obj);
+}
+
+let myInfo = {
+    name: "Farhan",
+    address: "Dhaka",
+    marks: [78, 66, 96, 72]
+};
+
+console.log("Outside function, before going to function-----------------------");
+console.log(myInfo);
+
+check1(myInfo);
+
+console.log("Outside function, After returning from function-----------------------");
+console.log(myInfo);
+
+
+
+// myInfo is changed because primitive data-type passes the memory reference. So, mutate করলে original-এও পরিবর্তন হয়। এখানে object বদলাওনি, object-এর ভিতরের data বদলেছ। কিন্তু function এর ভিতর Reassign করলে original-এ পরিবর্তন হয় না।
+
+
+
+function check2(ob){
+    ob = {                    // "এখন থেকে ob নতুন object-কে point করুক।"
+        name: "Shami Kabab",
+        price: 320
+    };
+    console.log("Inside function-----------------------");
+    console.log(ob);
+}
+
+let teacher = {
+    name: "Jalil Guljar",
+    address: "Karachi",
+};
+
+console.log("Outside function, before going to function-----------------------");
+console.log(teacher);
+
+check2(teacher);
+
+console.log("Outside function, After returning from function-----------------------");
+console.log(teacher);
+
+// teacher-এ কোনো পরিবর্তন হয়নি কারণ function এর ভিতরে mutate করে নাই, direct re-assign করেছে। Function এর ভিতরে Reassign করলে original object পরিবর্তন হয় না।
+
+
+
+
+
+// Example of mutation
+
+let arr1 = [1,2,3];
+let temp1 = arr1;
+temp1.push(35);
+console.log(arr1, temp1);
+
+
+
+// Example of Reassign
+
+let arr2 = [10, 20, 30];
+let temp2 = arr2;
+temp2 = [100, 200, 300, 400, 500];
+console.log(arr2, temp2);
+
+
