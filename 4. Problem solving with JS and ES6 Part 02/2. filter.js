102ba1f806ded64eabae568b869de24308bc9107
FarhanTips <farhanakbor02@gmail.com>
2026-08-06 21:59:47 +0600

    Students Filter

---
+++ 4. Problem solving with JS and ES6 Part 02/2. filter.js
@@ -0,0 +1,45 @@
+
+
+
+/*
+  Problem: Passing Students Filter
+  getPassingStudents(students, threshold) — return only the students
+  whose marks are greater than or equal to the given threshold.
+
+  Input:
+    students = [
+      { name: "Rafi", marks: 75 },
+      { name: "Karim", marks: 40 },
+      { name: "Utsho", marks: 60 },
+    ]
+    threshold = 60
+
+  Output:
+    [
+      { name: "Rafi", marks: 75 },
+      { name: "Utsho", marks: 60 },
+    ]
+*/
+
+// filter()-এ callback-এর return value array-তে যোগ হয় না। বরং JavaScript শুধু দেখে return value truthy নাকি falsy। যদি truthy হয় → element থাকবে। যদি falsy হয় → element বাদ যাবে।
+
+// map() : "তুমি যা return করবে, সেটাই নতুন array-এর element হবে।"
+// filter() : "তুমি যা return করবে, সেটা element হবে না; শুধু সিদ্ধান্ত নেবে element থাকবে নাকি বাদ যাবে।"
+const getPassingStudents = (arrStudents, threshold) => {
+    const filteredStudents = arrStudents.filter( (elem) => {
+        return elem.marks >= threshold;
+    });
+    return filteredStudents;
+};
+
+
+let students = [
+  { name: "Rafi", marks: 75 },
+  { name: "Karim", marks: 40 },
+  { name: "Utsho", marks: 60 },
+  { name: "Akash", marks: 59 },
+];
+console.log(getPassingStudents(students, 50));
+
+
+
