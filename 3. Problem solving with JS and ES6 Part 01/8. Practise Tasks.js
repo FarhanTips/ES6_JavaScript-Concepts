f1f23efdd1fb9191d1bcca73ffdb42db3296168c
FarhanTips <farhanakbor02@gmail.com>
2026-08-07 20:11:29 +0600

    Task 4 and 5

--- 3. Problem solving with JS and ES6 Part 01/8. Practise Tasks.js
+++ 3. Problem solving with JS and ES6 Part 01/8. Practise Tasks.js
@@ -27,7 +27,34 @@ const getLowestPrice = prices => {
     // Math.min() সাধারণত আলাদা আলাদা number চায়, কিন্তু আমার কাছে আছে একটা array, তাই আমি spread operator দিয়ে array-কে আলাদা আলাদা argument বানিয়েছি।
     // এটা internally হয়ে যায়: Math.min(340, 120, 560, 90); 
     return Math.min(...prices);
-}
+};
 
 let arr = [340, 120, 560, 90] 
 console.log(getLowestPrice(arr));
+
+
+
+
+// Task - 04
+const swapValues = (a, b) => {
+    [b, a] = [a, b];
+    return [a,b];
+};
+
+console.log(swapValues(5, 10) );
+
+
+
+
+// Task - 05
+const getEmployeeDept = employee =>{
+    let { job: {title, department}} = employee;
+    return {title, department};  // This is like magic, see output!
+};
+
+
+let info = { 
+    name: "Nadia", 
+    job: { title: "PM", department: "Product" } 
+};
+console.log(getEmployeeDept(info));
