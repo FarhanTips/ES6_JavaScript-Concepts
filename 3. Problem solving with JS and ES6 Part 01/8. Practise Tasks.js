cc955ca566eb893ed98a7608a6c66537c646ffe2
FarhanTips <farhanakbor02@gmail.com>
2026-08-07 19:43:30 +0600

    Task 1 to 3

---
+++ 3. Problem solving with JS and ES6 Part 01/8. Practise Tasks.js
@@ -0,0 +1,33 @@
+
+
+
+// Task - 01
+
+const calculateBill = (amount, taxRate = 0.05) => amount + (amount *taxRate);
+
+console.log(calculateBill(1000));
+console.log(calculateBill(1000, 0.10) );
+
+
+
+// Task - 02
+
+const display = obj => {
+    return `${obj.name}
+${obj.title} at ${obj.company}`;
+};
+
+console.log(display({ name: "Arif", title: "Developer", company: "Programming Hero" }));
+
+
+
+
+// Task - 03
+const getLowestPrice = prices => {
+    // Math.min() সাধারণত আলাদা আলাদা number চায়, কিন্তু আমার কাছে আছে একটা array, তাই আমি spread operator দিয়ে array-কে আলাদা আলাদা argument বানিয়েছি।
+    // এটা internally হয়ে যায়: Math.min(340, 120, 560, 90); 
+    return Math.min(...prices);
+}
+
+let arr = [340, 120, 560, 90] 
+console.log(getLowestPrice(arr));
